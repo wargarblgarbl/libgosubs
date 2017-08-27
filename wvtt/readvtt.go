1f3e1a8172329ea70c6db0b990829f62bb4a3145
Dobroslav Slavenskoj <c_dslave@qualcomm.com>
2017-08-26 22:55:36 -0400

    code cleanup

--- wvtt/readvtt.go
+++ wvtt/readvtt.go
@@ -1,4 +1,5 @@
 package webvtt
+
 import (
 	"bufio"
 	"os"
@@ -9,35 +10,35 @@ import (
 //CreateSubtitle creates a VTT formatted subtitle
 func CreateSubtitle(note bool, cue string, start string, end string, text []string, pos Position, haspos bool) *Subtitle {
 	return &Subtitle{
-		Note: note,
-		Cue:    cue,
-		Start: start,
-		End:   end,
+		Note:     note,
+		Cue:      cue,
+		Start:    start,
+		End:      end,
 		Position: pos,
-		Line:  text,
-		Haspos: haspos,
+		Line:     text,
+		Haspos:   haspos,
 	}
 }
 
 //CreatePosition creates a VTT subtiotle position
-func CreatePosition(vertical string, line int, posstring string, pos int, lpercent bool,  align string, size int) *Position {
+func CreatePosition(vertical string, line int, posstring string, pos int, lpercent bool, align string, size int) *Position {
 	return &Position{
-		Vertical: vertical,
-		Line: line,
-		Posstring: posstring,
-		Position: pos,
+		Vertical:    vertical,
+		Line:        line,
+		Posstring:   posstring,
+		Position:    pos,
 		Linepercent: lpercent,
-		Align: align,
-		Size: size,
+		Align:       align,
+		Size:        size,
 	}
 }
 
 //CreateStyle creates a style from a header string and an interface containing
 //the variables
 func CreateStyle(header string, variables map[string]interface{}) *Style {
-	return &Style {
+	return &Style{
 		Header: header,
-		Value: variables,
+		Value:  variables,
 	}
 }
 
@@ -57,9 +58,9 @@ func intit(in string) (out int) {
 }
 
 //a quick function to parse the timecode
-func parsetimecode(tc string)(start string, end string, pos bool){
+func parsetimecode(tc string) (start string, end string, pos bool) {
 	split := strings.Split(tc, " ")
- 	start = split[0]
+	start = split[0]
 	end = split[2]
 	if len(split) > 3 {
 		pos = true
@@ -68,7 +69,7 @@ func parsetimecode(tc string)(start string, end string, pos bool){
 }
 
 //a function to parse the positioning part of a timecode
-func parsepos(tc string)(vertical string, line int, posstring string, pos int, lpercent bool, align string, size int) {
+func parsepos(tc string) (vertical string, line int, posstring string, pos int, lpercent bool, align string, size int) {
 	split := strings.Split(tc, " ")
 	line = -2
 	pos = -2
@@ -77,9 +78,9 @@ func parsepos(tc string)(vertical string, line int, posstring string, pos int, l
 		b := strings.Split(a, ":")
 		for z, y := range b {
 			if strings.Contains(y, ",") {
-			 t :=	strings.Split(y, ",")
-			 b[z] = t[0]
-			 posstring = t[1]
+				t := strings.Split(y, ",")
+				b[z] = t[0]
+				posstring = t[1]
 			}
 		}
 		switch b[0] {
@@ -108,7 +109,7 @@ func parsepos(tc string)(vertical string, line int, posstring string, pos int, l
 }
 
 //check line - checks line to see if it's a header, a note, or a subtitle
-func checkline(line []string)(out int) {
+func checkline(line []string) (out int) {
 	for _, i := range line {
 		if strings.Contains(i, "WEBVTT") {
 			return 0
@@ -126,7 +127,7 @@ func checkline(line []string)(out int) {
 //LoadWebVtt loads a WebVtt file
 func LoadWebVtt(v *WebVtt, filepath string) error {
 	f, err := os.Open(filepath)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	scanner := bufio.NewScanner(f)
@@ -134,7 +135,7 @@ func LoadWebVtt(v *WebVtt, filepath string) error {
 	var file [][]string
 	var lines []string
 	for scanner.Scan() {
-		if scanner.Text() != ""   {
+		if scanner.Text() != "" {
 			lines = append(lines, scanner.Text())
 		} else {
 			file = append(file, lines)
@@ -176,17 +177,17 @@ func LoadWebVtt(v *WebVtt, filepath string) error {
 			} else {
 				v.Subtitle.Content = append(v.Subtitle.Content, *CreateSubtitle(false, i[0], start, end, i[2:], pos, haspos))
 			}
-			case 3:
-				var s Style
-				m := make(map[string]interface{})
-				s.Value = m
-				header := strings.Replace(i[1], "{", "", -1)
-				for _, h := range i[2:(len(i)-1)] {
-					j := strings.Split(h, ":")
-					s.Value[j[0]] = strings.Replace(j[1], ";", "", -1)
-				}
-				v.Styles = append(v.Styles, *CreateStyle(header, s.Value))
-				//Do the thing with the mapping our values
+		case 3:
+			var s Style
+			m := make(map[string]interface{})
+			s.Value = m
+			header := strings.Replace(i[1], "{", "", -1)
+			for _, h := range i[2:(len(i) - 1)] {
+				j := strings.Split(h, ":")
+				s.Value[j[0]] = strings.Replace(j[1], ";", "", -1)
+			}
+			v.Styles = append(v.Styles, *CreateStyle(header, s.Value))
+			//Do the thing with the mapping our values
 		}
 
 	}
@@ -195,7 +196,7 @@ func LoadWebVtt(v *WebVtt, filepath string) error {
 }
 
 //ParseWebVtt takes a filename and returns a WebVtt structure and any errors
-func ParseWebVtt(filename string)(*WebVtt, error) {
+func ParseWebVtt(filename string) (*WebVtt, error) {
 	v := &WebVtt{}
 	err := LoadWebVtt(v, filename)
 	if err != nil {
