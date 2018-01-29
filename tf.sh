7f03fb80e6de518d1533b4db0ed76b93d6a11a77
Dobroslav Slavenskoj <slava.slavenskoj@slalom.com>
2018-01-29 00:41:32 -0500

    0ci update

--- tf.sh
+++ tf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 for i in `find . | grep go`; do gofmt -w -s $i; done
-for i in  ass srt ttml wvtt mdvd; do cd $i; go test; cd ..; done
+for i in  ass srt ttml wvtt mdvd; do cd $i; go get -v -t -d ./...;  go test; cd ..; done
